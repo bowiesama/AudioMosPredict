17c8c4a1f758a9f9279a70b912876c4cf1f0cd77
bowchen <bowchen@cisco.com>
2019-05-15 09:59:16 +0800

    Update coreml

--- Audio_Mos_Predict/Audio_Mos_PreML.playground/Contents.swift
+++ Audio_Mos_Predict/Audio_Mos_PreML.playground/Contents.swift
@@ -1,14 +1,14 @@
 import CreateML
 import Foundation
 
-let audioData = try MLDataTable(contentsOf: URL(fileURLWithPath: "/Users/wme/git/bowchen/MachineLearningXcode/Audio_Mos_Predict/allInOne.csv"))
+let audioData = try MLDataTable(contentsOf: URL(fileURLWithPath: "/Users/wme/git/bowchen/AudioMosPredict/Audio_Mos_Predict/allInOne.csv"))
 let (trainingCSVData, testCSVData) = audioData.randomSplit(by: 0.8, seed: 0)
 
 let pricer = try MLRegressor(trainingData: audioData, targetColumn: "netMosAvg")
 //let pricer = try MLRandomForestRegressor(trainingData: houseData, targetColumn: "MEDV")
 let csvMetadata = MLModelMetadata(author: "bowchen", shortDescription: "A Model Predict Audio Mos", version: "2.0")
-try pricer.write(to: URL(fileURLWithPath: "/Users/wme/git/bowchen/MachineLearningXcode/Audio_Mos_Predict/Audio_mos.mlmodel"), metadata: csvMetadata)
+try pricer.write(to: URL(fileURLWithPath: "/Users/wme/git/bowchen/AudioMosPredict/Audio_Mos_Predict/Audio_mos.mlmodel"), metadata: csvMetadata)
 print(pricer)
-let audioTestData = try MLDataTable(contentsOf: URL(fileURLWithPath: "/Users/wme/git/bowchen/MachineLearningXcode/Audio_Mos_Predict/testdata.csv"))
+let audioTestData = try MLDataTable(contentsOf: URL(fileURLWithPath: "/Users/wme/git/bowchen/AudioMosPredict/Audio_Mos_Predict/testdata.csv"))
 let result = try pricer.predictions(from: audioTestData)
 print(result)
